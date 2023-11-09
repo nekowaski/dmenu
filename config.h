fcfbf225044a93dc426fc09716816dc144b33d2d
nekowaski <125260988+nekowaski@users.noreply.github.com>
2023-11-09 13:15:06 +0000

    Update config.h

--- config.h
+++ config.h
@@ -1,12 +1,11 @@
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-/* -fn option overrides fonts[0]; default X11 font or font set */
+static int topbar = 1;
 static const char *fonts[] = {
 	"JetBrains Mono:size=12",
 	"JoyPixels:pixelsize=12:antialias=true:autohint=true"
 };
 static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt      = NULL;
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
@@ -20,11 +19,6 @@ static const unsigned int alphas[SchemeLast][2] = {
 	[SchemeOut] = { fgalpha, bgalpha },
 };
 
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
-/*
- * Characters not considered part of a word while deleting words
- * for example: " /?\"&[]"
- */
 static const char worddelimiters[] = " ";
